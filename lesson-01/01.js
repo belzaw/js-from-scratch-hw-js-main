2845f650af92987abfdb8c767b959b06b25bc9a6
belzaw <belzaaaaaaa@gmail.com>
2025-07-27 13:54:01 +0200

    task 1

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,4 +2,11 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum
+let sum = 0;
+let counter = 1;
+while (counter <= 20){
+    sum = sum + counter;
+   // console.log(`${sum - counter} + ${counter} = ${sum}`);
+    counter++;
+}
+
