f188079ba2754e003774c69e82a2647919876269
belzaw <belzaaaaaaa@gmail.com>
2025-08-14 09:38:55 +0200

    lesson4 t1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,16 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === element) {
+      return true;
+    }
+    
+  }
+
+  return false;
+}
+
+
+
